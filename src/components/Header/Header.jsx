bb203913c7475626c95f46ac345a743cbe1a0ae5
Yosber Guerrero <yosbergc@gmail.com>
2024-10-09 00:25:31 -0400

    Added link to image, change title to spanish

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import CatLogo from '../../assets/logo-by-lucian-radu.png'
+import { Link } from 'react-router-dom'
 import { Menu } from '../Menu/Menu'
 import './header.css'
 
 function Header() {
     return (
         <header className='website-header'>
-            <img src={CatLogo} alt="Cat Logo from Lucian Radu" className='website-logo'/>
+            <Link to="/">
+                <img src={CatLogo} alt="Cat Logo from Lucian Radu" className='website-logo'/>
+            </Link>
             <Menu />
         </header>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <main>
-            <h1>The Cat Blog</h1>
+            <h1>El Blog del Gato</h1>
             <p>Bienvenido a <strong>The Cat Blog</strong> donde podrás ver los mejores blogs de gatitos en todo internet. Si eres amante de los gatos estás en el lugar correcto para aprender más sobre ellos y sobre todo para disfrutar de la mejor calidad de contenido.</p>
             <section className="buttons-container">
                 <button>Iniciar sesión</button>
